3c8b2996afc0cc1b4cb0c69dd1f4f67819c18910
Oleksii Samoilenko <osamoilenko@griddynamics.com>
2023-09-24 20:14:11 +0300

    second commit

---
+++ src/test/java/ua/testng/FirstTest.java
@@ -0,0 +1,13 @@
+package ua.testng;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+    @Test
+    public void Test_1(){
+        System.out.println("First Test_1()");
+        Assert.assertTrue(true);
+
+    }
+}
---
+++ src/test/java/ua/testng/GoogleTest.java
@@ -0,0 +1,24 @@
+package ua.testng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class GoogleTest {
+    @Test
+    public void SearchTest(){
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        Assert.assertEquals(driver.getTitle(), "Google");
+
+        driver.quit();
+    }
+}
